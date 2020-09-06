0e9ec5faaaa491e4527850e9e5adf1de11a1af0b
Josué Girón <benjamingiron1@gmail.com>
2020-09-06 02:30:40 -0600

    Fix formater caller

--- logger_test.go
+++ logger_test.go
@@ -6,7 +6,7 @@ import (
 
 func TestLogger_Output(t *testing.T) {
 	func() {
-		std.SetNewFormat(NewFormaterConfig(LstdDevFlags|JSONFormat, LstdProdFlags|Linfo|JSONFormat, "test"))
+		std.SetNewFormat(NewFormaterConfig(LstdDevFlags, LstdProdFlags|Linfo|JSONFormat, "test"))
 		std.DevelopmentMode()
 		stackTrace := GetStackTrace(2)
 		std.Output(1, infoPriority, "\"Hello world!", nil, stackTrace)
